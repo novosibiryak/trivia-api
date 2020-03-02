a1fb55a6ac18e22028c28625cdda0d34f7da5f68
Peter Shashkin <peter@shashkin.com>
2020-03-01 21:31:00 -0500

    some update

--- GetAllQuestions/index.js
+++ GetAllQuestions/index.js
@@ -4,6 +4,13 @@ module.exports = async function(context, req) {
   context.log("Request for all questions");
 
   context.res = {
-    body: questions
+    body: questions.map(q => {
+      return {
+        question: q.question,
+        answers: q.incorrect_answers.concat([q.correct_answer]).sort()
+      }
+    }
+
+    )
   };
 };
